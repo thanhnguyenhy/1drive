bd24e0d615fa4e46155c8b6e353531ac59b24a09
Bui Tien Thanh <132742555+thanhnguyenhy@users.noreply.github.com>
2023-07-21 23:08:18 +0700

    Update index.ts

--- src/pages/api/index.ts
+++ src/pages/api/index.ts
@@ -13,12 +13,6 @@ import { runCorsMiddleware } from './raw'
 const basePath = pathPosix.resolve('/', siteConfig.baseDirectory)
 const clientSecret = revealObfuscatedToken(apiConfig.obfuscatedClientSecret)
 
-/**
- * Encode the path of the file relative to the base directory
- *
- * @param path Relative path of the file to the base directory
- * @returns Absolute path of the file inside OneDrive
- */
 export function encodePath(path: string): string {
   let encodedPath = pathPosix.join(basePath, path)
   if (encodedPath === '/' || encodedPath === '') {
@@ -28,27 +22,19 @@ export function encodePath(path: string): string {
   return `:${encodeURIComponent(encodedPath)}`
 }
 
-/**
- * Fetch the access token from Redis storage and check if the token requires a renew
- *
- * @returns Access token for OneDrive API
- */
 export async function getAccessToken(): Promise<string> {
   const { accessToken, refreshToken } = await getOdAuthTokens()
 
-  // Return in storage access token if it is still valid
   if (typeof accessToken === 'string') {
     console.log('Fetch access token from storage.')
     return accessToken
   }
 
-  // Return empty string if no refresh token is stored, which requires the application to be re-authenticated
   if (typeof refreshToken !== 'string') {
     console.log('No refresh token, return empty access token.')
     return ''
   }
 
-  // Fetch new access token with in storage refresh token
   const body = new URLSearchParams()
   body.append('client_id', apiConfig.clientId)
   body.append('redirect_uri', apiConfig.redirectUri)
@@ -76,14 +62,7 @@ export async function getAccessToken(): Promise<string> {
   return ''
 }
 
-/**
- * Match protected routes in site config to get path to required auth token
- * @param path Path cleaned in advance
- * @returns Path to required auth token. If not required, return empty string.
- */
 export function getAuthTokenPath(path: string) {
-  // Ensure trailing slashes to compare paths component by component. Same for protectedRoutes.
-  // Since OneDrive ignores case, lower case before comparing. Same for protectedRoutes.
   path = path.toLowerCase() + '/'
   const protectedRoutes = siteConfig.protectedRoutes as string[]
   let authTokenPath = ''
@@ -98,28 +77,13 @@ export function getAuthTokenPath(path: string) {
   return authTokenPath
 }
 
-/**
- * Handles protected route authentication:
- * - Match the cleanPath against an array of user defined protected routes
- * - If a match is found:
- * - 1. Download the .password file stored inside the protected route and parse its contents
- * - 2. Check if the od-protected-token header is present in the request
- * - The request is continued only if these two contents are exactly the same
- *
- * @param cleanPath Sanitised directory path, used for matching whether route is protected
- * @param accessToken OneDrive API access token
- * @param req Next.js request object
- * @param res Next.js response object
- */
 export async function checkAuthRoute(
   cleanPath: string,
   accessToken: string,
   odTokenHeader: string
 ): Promise<{ code: 200 | 401 | 404 | 500; message: string }> {
-  // Handle authentication through .password
   const authTokenPath = getAuthTokenPath(cleanPath)
 
-  // Fetch password from remote file content
   if (authTokenPath === '') {
     return { code: 200, message: '' }
   }
@@ -132,9 +96,7 @@ export async function checkAuthRoute(
       },
     })
 
-    // Handle request and check for header 'od-protected-token'
     const odProtectedToken = await axios.get(token.data['@microsoft.graph.downloadUrl'])
-    // console.log(odTokenHeader, odProtectedToken.data.trim())
 
     if (
       !compareHashedToken({
@@ -145,7 +107,6 @@ export async function checkAuthRoute(
       return { code: 401, message: 'Password required.' }
     }
   } catch (error: any) {
-    // Password file not found, fallback to 404
     if (error?.response?.status === 404) {
       return { code: 404, message: "You didn't set a password." }
     } else {
@@ -172,104 +133,74 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
     return
   }
 
-  const { path = '/', raw = false, next = '', sort = '' } = req.query
+  let { path = '/', raw = false, next = '', sort = 'lastModifiedDateTime' } = req.query
 
-  res.setHeader('Cache-Control', apiConfig.cacheControlHeader)
-
-  if (path === '[...path]') {
-    res.status(400).json({ error: 'No path specified.' })
-    return
-  }
+  res.setHeader('Cache-Control', 'no-store')
 
   if (typeof path !== 'string') {
-    res.status(400).json({ error: 'Path query invalid.' })
+    res.status(400).send('Path must be a string.')
     return
   }
 
-  const cleanPath = pathPosix.resolve('/', pathPosix.normalize(path)).replace(/\/$/, '')
+  if (typeof raw !== 'boolean') {
+    raw = raw === 'true'
+  }
 
-  if (typeof sort !== 'string') {
-    res.status(400).json({ error: 'Sort query invalid.' })
-    return
+  if (typeof next !== 'string') {
+    next = next.toString()
   }
 
   const accessToken = await getAccessToken()
 
   if (!accessToken) {
-    res.status(403).json({ error: 'No access token.' })
-    return
-  }
-
-  const { code, message } = await checkAuthRoute(cleanPath, accessToken, req.headers['od-protected-token'] as string)
-  
-  if (code !== 200) {
-    res.status(code).json({ error: message })
-    return
-  }
-  
-  if (message !== '') {
-    res.setHeader('Cache-Control', 'no-cache')
-  }
-
-  const requestPath = encodePath(cleanPath)
-  const requestUrl = `${apiConfig.driveApi}/root${requestPath}`
-  const isRoot = requestPath === ''
-
-  if (raw) {
-    await runCorsMiddleware(req, res)
-    res.setHeader('Cache-Control', 'no-cache')
-
-    const { data } = await axios.get(requestUrl, {
-      headers: { Authorization: `Bearer ${accessToken}` },
-      params: {
-        select: 'id,@microsoft.graph.downloadUrl',
-      },
-    })
-
-    if ('@microsoft.graph.downloadUrl' in data) {
-      res.redirect(data['@microsoft.graph.downloadUrl'])
-    } else {
-      res.status(404).json({ error: 'No download url found.' })
-    }
+    res.status(401).send('Unauthorized.')
     return
   }
 
   try {
-    const { data: identityData } = await axios.get(requestUrl, {
-      headers: { Authorization: `Bearer ${accessToken}` },
-      params: {
-        select: 'name,size,id,lastModifiedDateTime,folder,file,video,image',
-      },
-    })
-
-    if ('folder' in identityData) {
-      const { data: folderData } = await axios.get(`${requestUrl}${isRoot ? '' : ':'}/children`, {
-        headers: { Authorization: `Bearer ${accessToken}` },
-        params: {
-          ...{
-            select: 'name,size,id,lastModifiedDateTime,folder,file,video,image',
-            $top: siteConfig.maxItems,
-          },
-          ...(next ? { $skipToken: next } : {}),
-          ...(sort ? { $orderby: sort } : {}),
-        },
-      })
-
-      const nextPage = folderData['@odata.nextLink']
-        ? folderData['@odata.nextLink'].match(/&\$skiptoken=(.+)/i)[1]
-        : null
-
-      if (nextPage) {
-        res.status(200).json({ folder: folderData, next: nextPage })
-      } else {
-        res.status(200).json({ folder: folderData })
-      }
-      return
-    }
-    res.status(200).json({ file: identityData })
-    return
-  } catch (error: any) {
-    res.status(error?.response?.code ?? 500).json({ error: error?.response?.data ?? 'Internal server error.' })
+    await runCorsMiddleware(req, res)
+  } catch (error) {
+    res.status(500).send('Internal server error.')
     return
   }
+
+  const cleanPath = path.replace(/\/+$/, '')
+
+  if (cleanPath.includes('//')) {
+    res.status(400).send('Invalid path.')
+    return
+  }
+
+  const { code, message } = await checkAuthRoute(cleanPath, accessToken, req.headers['x-od-token'])
+
+  if (code !== 200) {
+    res.status(code).send(message)
+    return
+  }
+
+  const requestUrl = `${apiConfig.driveApi}/root${encodePath(cleanPath)}`
+  const isRoot = cleanPath === '/'
+
+  const { data: folderData } = await axios.get(`${requestUrl}${isRoot ? '' : ':'}/children`, {
+    headers: { Authorization: `Bearer ${accessToken}` },
+    params: {
+      ...{
+        select: 'name,size,id,lastModifiedDateTime,folder,file,video,image',
+        $top: siteConfig.maxItems,
+      },
+      ...(next ? { $skipToken: next } : {}),
+      ...(sort ? { $orderby: sort } : {}),
+    },
+  })
+
+  let folderItems = folderData.value
+
+  if (!raw && !isRoot) {
+    const { data: parentData } = await axios.get(requestUrl, {
+      headers: { Authorization: `Bearer ${accessToken}` },
+    })
+    folderItems = [parentData, ...folderItems]
+  }
+
+  res.status(200).send(folderItems)
 }
