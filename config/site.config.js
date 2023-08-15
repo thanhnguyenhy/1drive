8ae049d8c456dfd303ad1803b9de29b20b663cde
Bui Tien Thanh <132742555+thanhnguyenhy@users.noreply.github.com>
2023-08-15 19:28:31 +0700

    Update site.config.js

--- config/site.config.js
+++ config/site.config.js
@@ -11,13 +11,13 @@ module.exports = {
 
   // [OPTIONAL] This is the website icon to the left of the title inside the navigation bar. It should be placed under the
   // /public directory of your GitHub project (not your OneDrive folder!), and referenced here by its relative path to /public.
-  icon: '/icons/sn.png',
+  icon: '/icons/64.png',
 
   // Prefix for KV Storage
   kvPrefix: process.env.KV_PREFIX || '',
 
   // The name of your website. Present alongside your icon.
-  title: "Sharengay OneDrive",
+  title: "OneDrive",
 
   // The folder that you are to share publicly with onedrive-vercel-index. Use '/' if you want to share your root folder.
   baseDirectory: '/Getlink',
@@ -38,7 +38,7 @@ module.exports = {
   // [OPTIONAL] The footer component of your website. You can write HTML here, but you need to escape double
   // quotes - changing " to \". You can write anything here, and if you like badges, generate some with https://shields.io
   footer:
-    'Made with ❤ by Sharengay.',
+    'Made with ❤ by Duongnamphong.',
 
   // [OPTIONAL] This is where you specify the folders that are password protected. It is an array of paths pointing to all
   // the directories in which you have .password set. Check the documentation for details.
@@ -50,16 +50,7 @@ module.exports = {
   // [OPTIONAL] This is an array of names and links for setting your social information and links.
   // In the latest update, all brand icons inside font awesome is supported and the icon to render is based on the name
   // you provide. See the documentation for details.
-  links: [
-    {
-      name: 'Facebook',
-      link: 'https://facebook.com/nqhuy.0701',
-    },
-    {
-      name: 'Onedrive',
-      link: 'https://sharengay1-my.sharepoint.com/:f:/g/personal/huynguyen_sharengay1_onmicrosoft_com/EgfRGZf7w_1DqtM_gEn2pzMBffHEaNpdnUsYrvtwC2nXVg',
-    }
-  ],
+  links: [],
 
   // This is a day.js-style datetime format string to format datetimes in the app. Ref to
   // https://day.js.org/docs/en/display/format for detailed specification. The default value is ISO 8601 full datetime
