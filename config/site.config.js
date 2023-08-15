7c8cc64a20521f93c9d214dd6fcf019a1cf654e3
Nguyen Quoc Huy <132742555+thanhnguyenhy@users.noreply.github.com>
2023-08-15 19:24:49 +0700

    Update site.config.js

--- config/site.config.js
+++ config/site.config.js
@@ -11,16 +11,16 @@ module.exports = {
 
   // [OPTIONAL] This is the website icon to the left of the title inside the navigation bar. It should be placed under the
   // /public directory of your GitHub project (not your OneDrive folder!), and referenced here by its relative path to /public.
-  icon: '/icons/64.png',
+  icon: '/icons/sn.png',
 
   // Prefix for KV Storage
   kvPrefix: process.env.KV_PREFIX || '',
 
   // The name of your website. Present alongside your icon.
-  title: "Duongnamphong OneDrive",
+  title: "Sharengay OneDrive",
 
   // The folder that you are to share publicly with onedrive-vercel-index. Use '/' if you want to share your root folder.
-  baseDirectory: '/Web',
+  baseDirectory: '/Getlink',
 
   // [OPTIONAL] This represents the maximum number of items that one directory lists, pagination supported.
   // Do note that this is limited up to 200 items by the upstream OneDrive API.
@@ -38,19 +38,28 @@ module.exports = {
   // [OPTIONAL] The footer component of your website. You can write HTML here, but you need to escape double
   // quotes - changing " to \". You can write anything here, and if you like badges, generate some with https://shields.io
   footer:
-    'Made by DuongNamPhong.',
+    'Made with ❤ by Sharengay.',
 
   // [OPTIONAL] This is where you specify the folders that are password protected. It is an array of paths pointing to all
   // the directories in which you have .password set. Check the documentation for details.
   protectedRoutes: ['/🌞 Private folder/u-need-a-password', '/🥟 Some test files/Protected route'],
 
   // [OPTIONAL] Use "" here if you want to remove this email address from the nav bar.
-  email: "",
+  email: 'mailto:huynguyen@yamcode.com',
 
   // [OPTIONAL] This is an array of names and links for setting your social information and links.
   // In the latest update, all brand icons inside font awesome is supported and the icon to render is based on the name
   // you provide. See the documentation for details.
-  links: [],
+  links: [
+    {
+      name: 'Facebook',
+      link: 'https://facebook.com/nqhuy.0701',
+    },
+    {
+      name: 'Onedrive',
+      link: 'https://sharengay1-my.sharepoint.com/:f:/g/personal/huynguyen_sharengay1_onmicrosoft_com/EgfRGZf7w_1DqtM_gEn2pzMBffHEaNpdnUsYrvtwC2nXVg',
+    }
+  ],
 
   // This is a day.js-style datetime format string to format datetimes in the app. Ref to
   // https://day.js.org/docs/en/display/format for detailed specification. The default value is ISO 8601 full datetime
